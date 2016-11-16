0b5aac0c84da3b5dc7496caa57aa690e57e2f6e6
Friedrich Menhorn <menhorn@mit.edu>
2016-11-16 14:38:32 -0500

    just added for merge

--- install_nowpac.sh
+++ install_nowpac.sh
@@ -5,12 +5,13 @@ cd ..
 [[ -d build ]] || mkdir build
 cd build
 
-cmake -DNOWPAC_INSTALL_PREFIX=/usr/local \
-      -DCMAKE_CXX_COMPILER=/usr/local/Cellar/gcc/5.3.0/bin/g++-5 \
-      -DCMAKE_C_COMPILER=/usr/local/Cellar/gcc/5.3.0/bin/gcc-5 \
+#cmake -DNOWPAC_INSTALL_PREFIX=/usr/local
+cmake -DCMAKE_CXX_COMPILER=g++ \
+      -DCMAKE_C_COMPILER=gcc \
+      -DCMAKE_BUILD_TYPE=Debug \
       -DNOWPAC_ENABLE_SHARED=ON \
       -DNOWPAC_ENABLE_TESTS=ON \
-      -DNOWPAC_ENABLE_DOXYGEN=ON \
+      -DNOWPAC_ENABLE_DOXYGEN=OFF \
       ../snowpac/
 
 make doc
