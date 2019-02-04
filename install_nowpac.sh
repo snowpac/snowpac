5cb3035eb65ca3baf4650c37b4221e937a97cb59
Friedrich Menhorn <menhorn@mit.edu>
2019-02-04 13:23:26 +0100

    Install nowpac update for mac.

--- install_nowpac.sh
+++ install_nowpac.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-clear
-#cd ..
-[[ -d build ]] || mkdir build
-cd build
+#clear
+cd ..
+#[[ -d build ]] || mkdir build
+#cd build
 
 #cmake -DNOWPAC_INSTALL_PREFIX=/usr/local
-cmake -DCMAKE_CXX_COMPILER=g++ \
-      -DCMAKE_C_COMPILER=gcc \
+cmake -DCMAKE_CXX_COMPILER=g++-7 \
+      -DCMAKE_C_COMPILER=gcc-7 \
       -DCMAKE_BUILD_TYPE=Debug \
       -DNOWPAC_ENABLE_SHARED=ON \
       -DNOWPAC_ENABLE_TESTS=ON \
       -DNOWPAC_ENABLE_DOXYGEN=OFF \
-      ../snowpac/
+      ./snowpac
 
 make doc
 make install -j4
